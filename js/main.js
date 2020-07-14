88a47d98b0c6d60dc3a3422b7cdc49fb817e44aa
Birteliu <zyi870213@gmail.com>
2020-07-14 22:46:36 +0800

    no message

--- js/main.js
+++ js/main.js
@@ -1,12 +1,39 @@
-///////////////// fixed menu on scroll for desktop
+// 選單下捲 js
+$('#menu-about').click(function (e) {
+    $('html, body').animate({
+        scrollTop: $('#about-section').offset().top
+    })
+})
+$('#menu-logo').click(function (e) {
+    $('html, body').animate({
+        scrollTop: $('#about-section').offset().top - 120
+    })
+})
+$('#menu-service').click(function (e) {
+    $('html, body').animate({
+        scrollTop: $('#service-section').offset().top - 110
+    })
+})
+$('#menu-products').click(function (e) {
+    $('html, body').animate({
+        scrollTop: $('#products-section').offset().top - 90
+    })
+})
+$('#menu-contact').click(function (e) {
+    $('html, body').animate({
+        scrollTop: $('#contact-section').offset().top - 110
+    })
+})
+
+// fixed menu on scroll for desktop
 if ($(window).width() > 992) {
     $(window).scroll(function () {
         if ($(this).scrollTop() > 150) {
-            $('#navbar_top').addClass("fixed-top");
+            $('#navbar_top').addClass("fixed-top shadow");
             // add padding top to show content behind navbar
             $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
         } else {
-            $('#navbar_top').removeClass("fixed-top");
+            $('#navbar_top').removeClass("fixed-top shadow");
             // remove padding top from body
             $('body').css('padding-top', '0');
         }
